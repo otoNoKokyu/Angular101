c4bd0bbbf3ac695e3f68ff71ea6f47e72559f875
Arkopal <arko466@gmail.com>
2023-07-19 23:37:25 +0530

    @hostlistener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  backgroundColor = 'bg-lightblue'; // Initial background color class
+
+  changeBackgroundColor(): void {
+    // Generate a random color class from Tailwind CSS
+    const randomColor = 'bg-red-500';
+    this.backgroundColor = randomColor;
+  }
 
 }
--- src/app/customDirective/setbackground.directive.ts
+++ src/app/customDirective/setbackground.directive.ts
@@ -1,18 +1,22 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appSetbackground]'
 })
-export class SetbackgroundDirective implements OnInit {
+export class SetbackgroundDirective  {
 
   constructor(private element: ElementRef, private renderer: Renderer2) {
 
    }
-   ngOnInit(){
-    setTimeout(()=>{
-      this.renderer.setProperty(this.element.nativeElement, 'textContent', 'hwjwbwrwlw')
-
-    }, 2000)
+   @HostListener('mouseenter') onmousehover(){
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', "red")
+    this.renderer.setStyle(this.element.nativeElement, 'transition', "0.2s")
    }
+   @HostListener('mouseleave') onmouseleave(){
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', "black")
+    this.renderer.setStyle(this.element.nativeElement, 'transition', "0.2s")
+
+   }
+
 
 }
