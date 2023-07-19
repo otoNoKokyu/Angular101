faf148a71183bcfeb8a8c819d8a90092ed1bfbb7
Arkopal <arko466@gmail.com>
2023-07-19 20:45:09 +0530

    main first comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import {BookServiceService} from './book-service.service'
 
-import { BooksComponent } from './books/books.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
   ],
-  providers: [BookServiceService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/books/book.model.ts
+++
@@ -1,4 +0,0 @@
-export class Book {
-    constructor(public title: string, public author: string, public publicationYear: number) {}
-  }
-  
